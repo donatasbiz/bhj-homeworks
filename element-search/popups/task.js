e6c98cf1774152344e2234164e538abac4da61a5
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-11-08 11:37:43 +0300

    Update task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,20 @@
+<head>
+        <script type="text/javascript">
+class="modal modal_active";
+
+window.open(#modal_main);
+
+closeButtons.modal(function(item) {
+
+   item.modal__close('click', function(e) {
+
+   });
+
+}); //
+function show(#modal_success) {
+    var shs = getAttribute("show-success");
+    }
+
+
+</script>
+    </head>
