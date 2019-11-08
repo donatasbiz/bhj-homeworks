d20220534f5a3e7ce848f27c20110cb62e024b04
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-11-08 11:51:22 +0300

    Update task.js

---
+++ element-search/menu/task.js
@@ -0,0 +1,37 @@
+<head>
+        <script type="text/javascript">
+class="menu menu_sub menu_active";
+
+$(document).on('click', '.menu__link', function() {
+  doStuff();
+});
+
+$(document).on('click', '.menu__item', function() {
+  doMoreStuff();
+});
+
+class="menu menu_link menu_active";
+
+href="https://netology.ru/clients" onclick="raz1(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
+<script>
+function raz1() {
+   alert('Скрипт сработал');
+};
+
+href="https://netology.ru/service" onclick="raz2(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
+<script>
+function raz2() {
+   alert('Скрипт сработал');
+};
+</script>
+
+href="https://netology.ru/wearecool" onclick="raz3(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
+<script>
+function raz3() {
+   alert('Скрипт сработал');
+};
+</script>
+
+
+</script>
+    </head>
