62557135c72aa9fc463bff47a8e45b4ade91de48
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-11-08 12:01:38 +0300

    Update task.js

---
+++ element-search/slider/task.js
@@ -0,0 +1,16 @@
+<head>
+        <script type="text/javascript">
+class="slider__item slider__item_active";
+
+slider__arrows.onclick = function() {
+    alert('');
+  };
+  
+  function slider__item() {
+    slides[slider__item].class = 'slider__arrows';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[slider__item].class = 'slider__arrows';
+}
+  
+</script>
+    </head>
