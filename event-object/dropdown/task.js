90c4f7ed6cfefeb9ded6a41f1c427b4ebbac4923
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-11-13 17:47:21 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,15 @@
+<script>
+class="dropdown__list dropdown__list_active";
+function toggle(dropdown__list);
+<select id="dropdown__list" >
+<option>JavaScript</option>
+<option>PHP</option>
+<option>Python</option>
+<option>Fortran</option>
+</select>
+
+ dropdown__list = function() {
+
+      return false;
+}
+</script>
