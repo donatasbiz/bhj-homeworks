2131cf5456cd2cb8a2b17d5eda465e6640c62a59
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-11-15 20:00:12 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,5 +1,4 @@
-<head>
-        <script type="text/javascript">
+
 class="menu menu_sub menu_active";
 
 var button = document.querySelector("button");
@@ -9,26 +8,22 @@ var button = document.querySelector("button");
 
 class="menu menu_link menu_active";
 
-href="https://netology.ru/clients" onclick="raz1(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
-<script>
+href="https://netology.ru/clients" onclick="event.preventDefault(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
+
 function raz1() {
    alert('Скрипт сработал');
 };
 
-href="https://netology.ru/service" onclick="raz2(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
-<script>
+href="https://netology.ru/service" onclick="event.preventDefault(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
+
 function raz2() {
    alert('Скрипт сработал');
 };
-</script>
 
-href="https://netology.ru/wearecool" onclick="raz3(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
-<script>
+
+href="https://netology.ru/wearecool" onclick="event.preventDefault(); return false;">Я ссылка, по мне не перейти, но я покажу сообщение</a>
+
 function raz3() {
    alert('Скрипт сработал');
 };
-</script>
 
-
-</script>
-    </head>
