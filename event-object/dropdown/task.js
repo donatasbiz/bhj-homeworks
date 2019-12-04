40bb00ac84d3a32c461cf4eb2d52b9b3cfc6c6c6
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-12-04 13:40:17 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,11 +1,12 @@
-var a = document.querySelectorAll('switch');
+var a = document.querySelectorAll('dropdown__link');
 
-letinput=document.getElementById('switch');
+letinput=document.getElementById('dropdown__value');
                                  input.onclick=function() {
     console.log(‘Buttonwasclicked’)
 }
 
 toggle(dropdown__list) {
+  document.getElementById("dropdown__list").className ="dropdown__list_active";
 <select id="dropdown__list" >
 <option>JavaScript</option>
 <option>PHP</option>
