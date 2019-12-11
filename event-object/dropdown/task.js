4a5fdf057d473363f7273da972140e24bf13b39a
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-12-11 21:39:41 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,16 +1,13 @@
-var a = document.querySelectorAll('dropdown__link');
-
-letinput=document.getElementById('dropdown__value');
-                                 input.onclick=function() {
-    console.log(‘Buttonwasclicked’)
-}
 
 let dropdownValue = document.querySelector('.dropdown__value');
 let list = document.querySelector('.dropdown__list');
 let links = document.querySelectorAll('a');
 
 document.querySelector('.dropdown__value').addEventListener('click', function(){ 
-let input = document.querySelector('.dropdown__value');
+if (.dropdown__value == click) {
+  alert (.dropdown__list_active);
+}
+  else { (.dropdown__list); }
 });
 
 dropdown__list_active.classList.add("dropdown__list ");
