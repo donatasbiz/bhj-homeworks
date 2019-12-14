b7d42aff9389a1c1617bc82af502b9f3ad499be1
donatasbiz <50598994+donatasbiz@users.noreply.github.com>
2019-12-14 13:57:04 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,11 +3,10 @@ let dropdownValue = document.querySelector('.dropdown__value');
 let list = document.querySelector('.dropdown__list');
 let links = document.querySelectorAll('a');
 
-document.querySelector('.dropdown__value').addEventListener('click', function(){ 
-if (.dropdown__value == click) {
-  alert (.dropdown__list_active);
-}
-  else { (.dropdown__list); }
-});
+dropdownValue.addEventListener('click', function(){ 
+if (list.className === 'dropdown__list') { 
+  list.className = 'dropdown__list dropdown__list_active'; 
+} else { list.className = 'dropdown__list'; 
+       }
 
 dropdown__list_active.classList.add("dropdown__list ");
